00e0d69564abc5e18c177ceaf063ac618672f421
mjuferdi <juferdi.mario@yahoo.com>
2018-05-27 17:26:02 +0200

    Initial Commit. UI Builder Home

---
+++ CanWeEat/BlockedIngredientsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BlockedIngredientsViewController.swift
+//  CanWeEat
+//
+//  Created by Mario Muhammad on 27.05.18.
+//  Copyright © 2018 Mario Muhammad. All rights reserved.
+//
+
+import UIKit
+
+class BlockedIngredientsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
